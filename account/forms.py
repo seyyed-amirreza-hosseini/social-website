e50d55a7eab12152c9270f2a67c3543581f13412
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-18 14:16:32 +0330

    enable edit functionality for user and profile

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -22,3 +23,14 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords don NOT match!")
         return cd['password2']
             
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['data_of_birth', 'photo']
--- account/urls.py
+++ account/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    
     
 ]
