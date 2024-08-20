8125e2389f71c15c9d71bb632f4cd2c9050142f9
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-20 08:42:16 +0330

    override save method of Model Form

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include('account.urls')),
     path("social-auth/", include('social_django.urls'), name='social'),
+    path("images/", include('images.urls')),
 ]
 
 if settings.DEBUG:
--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,8 @@
+from typing import Any
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from .models import Image
 
 
@@ -16,5 +20,17 @@ class ImageCreateForm(forms.ModelForm):
         extention = url.rsplit('.', 1)[1].lower()
         if extention not in valid_extentions:
             raise forms.ValidationError('The given URL does not match valid image extentions.')
+        return url
 
-        return url        
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super.save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f"{name}.{extension}"
+        # download image from a giving URL
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image    
