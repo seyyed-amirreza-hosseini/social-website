8d53da7e6612ef4a56ab4953d5557a985ee42ed3
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-08-20 16:43:20 +0330

    add asynchronous actions with JavaScript

--- images/views.py
+++ images/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
+
+
+
 
 
 @login_required
@@ -26,3 +31,21 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
